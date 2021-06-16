78c2ee2c7098d2feda666f6e9f12ea3cbb208ac3
stefanToljic <85561921+stefan-toljic@users.noreply.github.com>
2021-06-16 12:30:03 +0000

    Switched from while to for and reduced the number of times list_size is used.

--- main.py
+++ main.py
@@ -18,25 +18,20 @@ split_input = user_input.split(" ")
 list_size = len(split_input)
 
 def more_than_4(list_size):
-    reverse_order = []
+    reverse_input = []
 
-    while list_size > 0:
-        list_size -= 1
-        reverse_order.append(list_size)
-
-    reverse_input = [split_input[i] for i in reverse_order]
-
+    for n in range(list_size):
+        reverse_input.append(split_input[(n + 1) * (-1)]) 
+    
     output = " ".join(reverse_input)
 
     print(f"\n- {output}")
-
-list_size = len(split_input)
-
+    
 if list_size > 4:
     more_than_4(list_size)
 else:
-    print("\n- {}".format(split_input[list_size - 1]))
+    print("\n- {}".format(split_input[-1]))
 
-# ----------------------------------------------------
+# -----------------------------------------
 
 
